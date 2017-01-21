5f395663592ba4ba9473d6f6d29110d4d02765a6
EnJiang <315067671@qq.com>
2017-01-21 19:29:37 +0800

    random

---
+++ randomPick.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+
+import random
+import copy
+from Game import *
+
+def getRand(last):
+  color = random.randint(1,6)
+  while color==last:
+    color = random.randint(1,6)
+  return color
+
+while 1:
+  game = Game()
+  last = 0
+  while not game.isOver():
+    color = getRand(last)
+    last = color
+    game.change(color)
+  print game.step,game.allStep
