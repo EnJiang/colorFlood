20c8a0e8d9fc1c6b5097d465b3cd9200ce67fe81
EnJiang <315067671@qq.com>
2018-04-27 17:39:58 +0800

    update
    use conv net

--- dqn.py
+++ dqn.py
@@ -2,7 +2,7 @@ import numpy as np
 import gym
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten, Conv2D
 from keras.optimizers import Adam
 
 from rl.agents.dqn import DQNAgent
@@ -37,9 +37,9 @@ class MyPolicy(EpsGreedyQPolicy):
                 action = np.random.random_integers(0, nb_actions - 1)
         else:  # warming up done
             rand = np.random.uniform()
-            if 0 < rand < 0.05:
+            if 0 < rand < 0.05: # 5% rand
                 action = np.random.random_integers(0, nb_actions - 1)
-            elif 0.05 < rand < 0.2:
+            elif 0.05 < rand < 0.15: # 10% greedy
                 try:
                     action = greedy(self.env.game, 1)[0] - 1
                 except:
@@ -73,17 +73,18 @@ ENV_NAME = 'colorflood'
 env = Env()
 nb_actions = 6
 # Next, we build a very simple model.
+
 model = Sequential()
-model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu", input_shape=(1, ) + env.observation_space.shape,
+                 data_format="channels_first"))
+# model.add(MaxPool2D(2, 2)),
+# model.add(Conv2D(filters=8, kernel_size=(3, 3),
+#                  activation="relu", data_format="channels_first"))
+# model.add(MaxPool2D(2, 2))
+model.add(Conv2D(filters=64, kernel_size=(2, 2),
+                 activation="relu", data_format="channels_first"))
+model.add(Flatten())
 model.add(Dense(1024))
-model.add(Activation('relu'))
-# model.add(Dense(4096))
-# model.add(Activation('relu'))
-# model.add(Dense(1024))
-# model.add(Activation('relu'))
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dense(64))
 model.add(Activation('relu'))
@@ -91,20 +92,21 @@ model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dense(nb_actions))
 model.add(Activation('linear'))
-print(model.summary())
+model.summary()
+
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
 memory = SequentialMemory(limit=50000, window_length=1)
 policy = MyPolicy(env)
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,
-               target_model_update=1e-2, policy=policy, enable_dueling_network=True)
+               target_model_update=1e-3, policy=policy, enable_dueling_network=True)
 dqn.compile(Adam(lr=1e-4), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=200000, visualize=False, verbose=2)
+dqn.fit(env, nb_steps=500000, visualize=False, verbose=1)
 
 # After training is done, we save the final weights.
 dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
