b0c9d4af18858b6888ad63c176c6590667b0cb78
EnJiang <315067671@qq.com>
2018-04-28 14:20:55 +0800

    fix bug

--- dqn.py
+++ dqn.py
@@ -18,7 +18,7 @@ import random
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('load', type=bool, help='load file or not', default=False)
+parser.add_argument('load', type=int, help='load file or not', default=0)
 args = parser.parse_args()
 DEBUG = False
 
