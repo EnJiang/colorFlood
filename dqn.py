37d22a2d89707704c6bf2eb815cc7bdbe9ffeb39
EnJiang <315067671@qq.com>
2018-04-28 09:32:37 +0800

    update
    testing...

--- dqn.py
+++ dqn.py
@@ -12,6 +12,10 @@ from rl.memory import SequentialMemory
 from env import Env
 from greedy import greedy
 
+import random
+
+DEBUG = False
+
 class MyPolicy(EpsGreedyQPolicy):
     def __init__(self, env, eps=.5):
         super().__init__()
@@ -30,15 +34,21 @@ class MyPolicy(EpsGreedyQPolicy):
         assert q_values.ndim == 1
         nb_actions = q_values.shape[0]
 
-        rand = np.random.uniform()
+        rand = random.random()
         if 0 < rand < 0.05: # 5% rand
             action = np.random.random_integers(0, nb_actions - 1)
+            if DEBUG: 
+                print("rand")
         elif 0.05 < rand < 0.15: # 10% greedy
+            if DEBUG:
+                print("geedy")
             try:
                 action = greedy(self.env.game, 1)[0] - 1
             except:
                 action = np.random.random_integers(0, nb_actions - 1)
         else:
+            if DEBUG:
+                print("net")
             action = np.argmax(q_values)
 
         return action
@@ -53,15 +63,15 @@ nb_actions = 6
 # Next, we build a very simple model.
 
 model = Sequential()
-model.add(Conv2D(filters=128, kernel_size=(2, 2), activation="relu", input_shape=(1, ) + env.observation_space.shape,
+model.add(Conv2D(filters=64, kernel_size=(2, 2), activation="relu", input_shape=(1, ) + env.observation_space.shape,
                  data_format="channels_first"))
 # model.add(MaxPool2D(2, 2)),
 # model.add(Conv2D(filters=8, kernel_size=(3, 3),
 #                  activation="relu", data_format="channels_first"))
 # model.add(MaxPool2D(2, 2))
-model.add(Conv2D(filters=64, kernel_size=(2, 2),
+model.add(Conv2D(filters=32, kernel_size=(2, 2),
                  activation="relu", data_format="channels_first"))
-model.add(Conv2D(filters=64, kernel_size=(2, 2),
+model.add(Conv2D(filters=32, kernel_size=(2, 2),
                  activation="relu", data_format="channels_first"))
 model.add(Flatten())
 model.add(Dense(512))
@@ -81,8 +91,8 @@ model.summary()
 # even the metrics!
 memory = SequentialMemory(limit=50000, window_length=1)
 policy = MyPolicy(env)
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=1000,
-               target_model_update=1e-3, policy=policy, enable_dueling_network=True)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10000,
+               target_model_update=1e-3, policy=policy, enable_dueling_network=False)
 dqn.compile(Adam(lr=1e-4), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
@@ -90,7 +100,7 @@ dqn.compile(Adam(lr=1e-4), metrics=['mae'])
 # Ctrl + C.
 
 # dqn.load_weights('dqn_{}_weights.h5f'.format(ENV_NAME))
-dqn.fit(env, nb_steps=500000, visualize=False, verbose=2)
+dqn.fit(env, nb_steps=250000, visualize=False, verbose=2)
 dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
