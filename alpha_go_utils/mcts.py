02e091a573855bfed8b49903b79ed6c2ea86c22a
EnJiang <315067671@qq.com>
2018-05-15 09:33:28 +0800

    alpha go in progress
    might push it further in this summer when I am free

--- alpha_go_utils/mcts.py
+++ alpha_go_utils/mcts.py
@@ -1,13 +1,15 @@
 import numpy as np
 from copy import deepcopy
+from math import sqrt
 
 class Node(object):
-    def __init__(self, game):
+    def __init__(self, game, policy, c=1):
         self.game = game
         self.N = 0
-        self.N_a = [0 for _ in range(6)]
-        self.q_values = [0 for _ in range(6)]
-
+        self.N_a_list = [0 for _ in range(6)]
+        self.q_a_list = [0 for _ in range(6)]
+        self.p_a_list = policy
+        self.c = 1
         self.children_hash_list = self.cal_child_node_hash()
 
     def cal_child_node_hash(self):
@@ -18,9 +20,19 @@ class Node(object):
             tmp_game.change(a)
             hash_list[a] = tmp_game.hash_string()
 
-    @property
-    def ucb_list(self):
-        pass
+    def ucb_list(self, tree):
+        q_a_list = self.q_a_list
+        p_a_list = self.p_a_list
+        N_a_list = self.N_a_list
+        return [
+            self.cal_ucb(q_a, p_a, N_a)
+            for q_a, p_a, N_a in zip(
+                q_a_list, p_a_list, N_a_list
+            )
+        ]
+
+    def cal_ucb(self, q_a, p_a, N_a):
+        return q_a + self.c * p_a * sqrt(self.N) / (1 + N_a)
 
     def __repr__(self):
         return self.game.hash_string()
@@ -42,16 +54,18 @@ class MCTS(object):
 
     def search_down(self, root):
         chian = []
+        next_root = None
         while self.all_children_in_tree(root):
-            chian.append(repr(root))
             # now select by ucb
-            root = self.next_search_down_node(root)
+            next_root, child_index = self.next_search_down_node(root)
+            chian.append((hash(root), child_index))
+            root = next_root
         
         # now the root node is not full
         child = self.select_and_create_nonexist_child(root)
-        p = self.predict_value(child)
+        v = self.predict_value(child.game)
 
-        self.back_prop(chian, p)
+        self.back_prop(chian, v)
 
     def all_children_in_tree(self, root):
         return all(
@@ -69,15 +83,21 @@ class MCTS(object):
                 action = i + 1
                 tmp_game = deepcopy(root.game)
                 tmp_game.change(action)
-                child = Node(tmp_game)
+                child = Node(tmp_game, self.predict_policy(tmp_game))
                 self.tree[hash(child)] = child
         return child
 
     def next_search_down_node(self, root):
-        ucb_list = root.ucb_list
+        ucb_list = root.ucb_list(self.tree)
         child_index = np.argmax(ucb_list)
         child_hash = root.children_hash_list[child_index]
-        return self.tree[child_hash]
+        return self.tree[child_hash], child_index
+
+    def predict_value(self, game):
+        pass
+
+    def predict_policy(self, game):
+        pass
 
     @property
     def pi(self):
--- alpha_go_zero.py
+++ alpha_go_zero.py
@@ -2,6 +2,7 @@ from alpha_go_utils.data import *
 import numpy as np
 
 if __name__ == "__main__":
-    filename = generate_greedy()
-    tdata = np.load("./data/greedy_1/%s" % filename)
-    print(tdata["xs"])
+    # filename = generate_greedy()
+    # tdata = np.load("./data/greedy_1/%s" % filename)
+    # print(tdata["xs"])
+    pass
