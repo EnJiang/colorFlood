5eb4d6e319dcdb878262127644cbcefb3e366fd1
EnJiang <315067671@qq.com>
2018-06-09 16:42:14 +0800

    to generate data

--- alpha_go_zero.py
+++ alpha_go_zero.py
@@ -6,31 +6,32 @@ import torch.nn as nn
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    # filename = generate_greedy(data_num=1000)
-    tdata = np.load("./1528531033.npz")
-    xs = tdata["xs"]
-    ys = tdata["ys"]
-    xs = np.reshape(xs, (1305, 10, 4, 6, 6))
-    ys = np.reshape(ys, (1305, 10, 7))
+    filename = generate_greedy(data_num=100000)
+    # tdata = np.load("./1528531033.npz")
+    # xs = tdata["xs"]
+    # ys = tdata["ys"]
+    # xs = np.reshape(xs, (1305, 10, 4, 6, 6))
+    # ys = np.reshape(ys, (1305, 10, 7))
 
-    model = ConvNet().cuda()
-    model.train()
+    # model = ConvNet().cuda()
+    # model.train()
 
-    criterion = nn.MSELoss()
-    optimizer = optim.SGD(model.parameters(), lr=1e-3,
-                        momentum=0.9, weight_decay=5e-4)
+    # criterion = nn.MSELoss()
+    # optimizer = optim.SGD(model.parameters(), lr=1e-3,
+    #                     momentum=0.9, weight_decay=5e-4)
 
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in tqdm(enumerate(zip(xs, ys))):
-        inputs, targets = torch.FloatTensor(inputs), torch.FloatTensor(targets)
-        inputs, targets = inputs.cuda(), targets.cuda()
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
+    # for _ in range(100):
+    #     train_loss = 0
+    #     correct = 0
+    #     total = 0
+    #     for batch_idx, (inputs, targets) in tqdm(enumerate(zip(xs, ys))):
+    #         inputs, targets = torch.FloatTensor(inputs), torch.FloatTensor(targets)
+    #         inputs, targets = inputs.cuda(), targets.cuda()
+    #         optimizer.zero_grad()
+    #         outputs = model(inputs)
+    #         loss = criterion(outputs, targets)
+    #         loss.backward()
+    #         optimizer.step()
 
-        train_loss += loss.item()
-    print(train_loss)
+    #         train_loss += loss.item()
+    #     print(train_loss)
