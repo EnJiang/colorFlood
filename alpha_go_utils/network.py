b6fa7fa9f3efa36ad5c5c5480e10c91bb2bdebbc
EnJiang <315067671@qq.com>
2018-06-13 12:57:57 +0800

    debugging

--- alpha_go_utils/network.py
+++ alpha_go_utils/network.py
@@ -57,6 +57,7 @@ class MyLoss(nn.Module):
 
     def forward(self, predict, real):
         target_1 = real[:, : -1].long()
+        print(target_1)
         target_2 = real[:, -1:]
         loss_1 = self.loss_func_1(predict[:, : -1], target_1)
         loss_2 = self.loss_func_2(predict[:, -1: ], target_2)
