3f2a0a3f55ece1c287bcba050daf21b8acda89a4
EnJiang <315067671@qq.com>
2018-06-13 16:10:52 +0800

    testing

--- test.py
+++ test.py
@@ -28,25 +28,29 @@ if __name__ == "__main__":
 
 
     e = Env(size=6)
-    done = False
-    obs = e.reset()
-    obs = np.reshape(obs, (1, 4, 6, 6))
-    obs = torch.FloatTensor(obs).cuda()
-    while not done:
-        output = model(obs)
-        output = output.cpu().data.numpy()[0]
-        pi = output[: 6]
-        a = output[-1]
 
-        action_greedy = greedy(e.game, 1)[0] - 1
-        action_pi = np.argmax(pi)
-
-        print(e.game)
-        print(action_greedy, action_pi, a)
-        print(pi)
-
-        next_obs, reward, done, _ = e.step(action_greedy)
-        obs = next_obs
+    for _ in range(10):
+        done = False
+        obs = e.reset()
         obs = np.reshape(obs, (1, 4, 6, 6))
         obs = torch.FloatTensor(obs).cuda()
-    print("\n\n\n\n\n\n")
+        while not done:
+            output = model(obs)
+            output = output.cpu().data.numpy()[0]
+            pi = output[: 6]
+            a = output[-1]
+
+            action_greedy = greedy(e.game, 1)[0] - 1
+            action_pi = np.argmax(pi)
+
+            # print(e.game)
+            # print(action_greedy, action_pi, a)
+            # print(pi)
+
+            next_obs, reward, done, _ = e.step(action_pi)
+            obs = next_obs
+            obs = np.reshape(obs, (1, 4, 6, 6))
+            obs = torch.FloatTensor(obs).cuda()
+
+        print(e.game.step)
+        print("\n\n\n\n\n\n")
