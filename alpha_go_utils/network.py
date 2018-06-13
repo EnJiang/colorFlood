2f9da6a6c2eebe01c75cba2a1eef5d75adf89481
EnJiang <315067671@qq.com>
2018-06-13 12:56:43 +0800

    patch my loss function

--- alpha_go_utils/network.py
+++ alpha_go_utils/network.py
@@ -56,7 +56,7 @@ class MyLoss(nn.Module):
         self.loss_func_2 = nn.MSELoss()
 
     def forward(self, predict, real):
-        target_1 = real[:, : -1].Long()
+        target_1 = real[:, : -1].long()
         target_2 = real[:, -1:]
         loss_1 = self.loss_func_1(predict[:, : -1], target_1)
         loss_2 = self.loss_func_2(predict[:, -1: ], target_2)
