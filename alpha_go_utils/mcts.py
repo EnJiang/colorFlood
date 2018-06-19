07a85ea5afb2f3a491285e84f5301dc437274d98
EnJiang <315067671@qq.com>
2018-06-19 19:14:39 +0800

    smaller c

--- alpha_go_utils/mcts.py
+++ alpha_go_utils/mcts.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import random
 
 class Node(object):
-    def __init__(self, env, policy, c=15):
+    def __init__(self, env, policy, c=6):
         self.env = env
         self.N = 0
         self.N_a_list = [0 for _ in range(6)]
